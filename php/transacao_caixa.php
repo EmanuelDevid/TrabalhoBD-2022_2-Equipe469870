4bf0be519e77dc348c29d8d2ae62a0b5319ac1ef
EmanueDevid <emanueldavid150@gmail.com>
2022-12-04 17:24:56 -0300

    fazendo a lógica da transação do caixa

--- php/transacao_caixa.php
+++ php/transacao_caixa.php
@@ -2,18 +2,14 @@
     include_once("../php/validador_acesso.php");
     include_once("../php/conexao.php");
 
-    $cpf = $_SESSION["login_usuário"];
-
-    //pegando o num_Conta informado pelo usuário
-    $num_conta = $_SESSION['num_conta'];
-
-    //convertendo o valor digitado pelo cliente em float
+    //convertendo o valor digitado pelo caixa em float
     $valor_transacao = floatval(str_replace(',', '.', $_POST['valor_transacao']));
     $tipo_transacao = $_POST['select_transacoes'];
+    $num_conta = $_POST['num_conta'];
 
     //Realizando uma transação na conta cujo o num_conta foi informado pelo usuário
     $stmt = $conexao->prepare("INSERT INTO Transacao(tipo_transacao, data_hora, valor, Contas_num_conta) VALUES ('$tipo_transacao', sysdate(), '$valor_transacao', '$num_conta')");
     if($stmt->execute()){
-        header('Location: ../paginas/cliente.php');
+        header('Location: ../paginas/caixa.php');
     }
 ?>
